551e1a153a75f70fe3f9395e0346968b0a10b0aa
ahsanMah <mahmood.ahsan@outlook.com>
2019-06-21 17:57:16 -0400

    Model weights are now saved after training

--- ClusterPipeline.py
+++ ClusterPipeline.py
@@ -37,6 +37,9 @@ class ClusterPipeline:
         def transform(self, labels):
             return self.enc.transform(labels.values.reshape(-1,1))
     
+    MODELDIR = "models/"
+    INITFILE = MODELDIR+"init.h5"
+    BASENAME = MODELDIR+"dnn"
 
     def __init__(self, model, train_set, val_set,
                 analyzer="lrp",
@@ -47,15 +50,26 @@ class ClusterPipeline:
         data: [training_data, validation_data]
         """
         self.model = model
+        self.model.save_weights(self.INITFILE)
+
         self.train_set = self.Dataset(*train_set)
         self.val_set = self.Dataset(*val_set)
 
         self.hot_encoder = self.dfHotEncoder()
         self.hot_encoder.fit(self.train_set.labels)
         self.history = None
-    
-    def train(self, model, X, y, batch_size, epochs, X_test=[], y_test=[], verbose=1, plot=True):
-        
+        self.model_zoo = []
+
+        # Fold counter to keep track of which model is being trained
+        # Each fold should train a fresh model and save its weights
+        self.foldctr = 0
+
+    def train(self, model, X, y, batch_size, epochs, X_test=[], y_test=[], verbose=1):
+        """
+        Trains a model using keras API, after scaling the data
+        """
+        self.model.load_weights(self.INITFILE)
+
         ZScaler = StandardScaler().fit(X)
         
         X_train = ZScaler.transform(X)
@@ -64,14 +78,23 @@ class ClusterPipeline:
         history = model.fit(X_train, y_train,
                         epochs=epochs, batch_size = batch_size, verbose=verbose)
         
-    #     if plot: plot_history(history)
-        
+        self.model.save_weights(
+            "{basename}_{id}.h5".format(basename=self.BASENAME, id=self.foldctr)
+        )
+
         return history, ZScaler
 
-
+    def cross_validation(slef, num_folds = 10,):
+        
+        # Populate Zoo here perhaps
+        # Use the h5 weight files...
+        # self.model_zoo.append(model)
+        pass
 
     def train_model(self, batch_size=20, epochs=200, cross_validation=False):
         
+        self.model_zoo = []
+
         if not cross_validation:
             X_train, y_train = self.train_set.features, self.train_set.labels
             history, ZScaler = self.train(self.model, X_train,y_train, batch_size=batch_size, epochs=epochs)
