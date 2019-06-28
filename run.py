08c57881e20b4597b62ee9849e893b2113f12ec8
ahsanMah <mahmood.ahsan@outlook.com>
2019-06-28 17:31:02 -0400

    - Code that should not be copied over to child processes has to be put inside a main guard - The model construction was intentionally put outside the guard to allow each child process a fresh keras graph

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 #%%
 
-# Magic line to force reload all modules when this cell is run muktiple times
-%load_ext autoreload
-%autoreload 2
+# Magic line to force reload all modules when this cell is run multiple times
+# %load_ext autoreload
+# %autoreload 2
 
 from ClusterPipeline import ClusterPipeline
-# reload(sys.modules["ClusterPipeline"])
-
 from common_imports import *
 from helper import *
 
@@ -39,18 +37,39 @@ def build_dnn(num_features, num_nodes = 16, depth = 2, num_labels=2, activation 
     
     return nn
 
-#%%
-original_data, modded_samples, training_labels, original_labels = simulate_blobs(class_size=1000)
-# Separating a hold out set that will be used for validation later
-X_train, y_train, X_test, y_test, y_original, X_valid, y_valid, y_valid_original = get_train_test_val(modded_samples, original_labels, training_labels)
 
 #%%
-num_features = modded_samples.shape[1]
-nn = build_dnn(num_features, depth=2)
-#%%
-pipeline = ClusterPipeline(nn, [X_train, y_train], [X_test,y_test])
+nn = build_dnn(num_features=2, depth=2)
+
+if __name__ == "__main__":
+    original_data, modded_samples, training_labels, original_labels = simulate_blobs(class_size=1000)
+
+    # Separating a hold out set that will be used for validation later
+    X_train, y_train, X_test, y_test, y_original, X_valid, y_valid, y_valid_original = get_train_test_val(modded_samples, original_labels, training_labels)    
+    num_features = modded_samples.shape[1]
+    # print("Model in parent proc:", nn)
+    # print(nn.summary())
+
+    pipeline = ClusterPipeline(nn, [X_train, y_train], [X_test,y_test])
+    pipeline.train_model(batch_size=20,epochs=50, cross_validation=True)
+
 
 #%%
-pipeline.train_model(20,10)
+# from multiprocess import Pool
+
+
+# num_procs = 4
+
+# if __name__ == "__main__":
+#     from multiprocessing import Pool
+#     POOL = Pool(num_procs)
+#     print("Declared pool worker...")
+
+
+# %time 
+
+
+#%%
+# POOL.map(lambda x : x**x, range(3))
 
 #%%
